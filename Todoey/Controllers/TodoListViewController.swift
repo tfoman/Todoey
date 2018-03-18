8552e0e375543fb617380ad7e5091ee8f8b438a3
Tommy Öman <tommy@oeman.se>
2018-03-18 22:49:36 +0100

    Database handling sqlite in CoreData format implemented

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,20 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
 
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
-        
-        // Do any additional setup after loading the view, typically from a nib.
-
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+                
         loadItems()
 
     }
@@ -45,17 +46,20 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-    //MARK - TableView delegate methods
+    //MARK: - TableView delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //MARK - Add new items
+    //MARK: - Add new items
     
     @IBAction func AddButtonPressed(_ sender: Any) {
 
@@ -65,8 +69,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What is going to happen once the user clicks the Add Item button on our UIAlert
-            let newItem = Item(t_ext: todoeyText.text!, ch_ecked: false)
             
+
+            let newItem = Item(context: self.context)
+            newItem.title = todoeyText.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             self.saveItems()
 
@@ -82,32 +89,42 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    //MARK - Model Manupulation Methods
+    //MARK: - Model Manupulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print ("Error encoding item array, \(error) ")
+            print("Error saving context \(error)")
         }
-        tableView.reloadData()
+        self.tableView.reloadData()
 
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch {
-                print("Error decoding item array, \(error)")
-            }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+           print("Error fetching data from context \(error)")
         }
-        
     }
     
 }
 
+//MARK: - Search bar methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+        loadItems(with: request)
+        
+
+    }
+    
+
+}
