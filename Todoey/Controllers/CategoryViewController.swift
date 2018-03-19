b59d1b34db0bbba2fb1c255729e411b90ab0fb07
Tommy Öman <tommy@oeman.se>
2018-03-19 23:59:48 +0100

    Before starting my own work in the app

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Tommy Öman on 2018-03-18.
+//  Copyright © 2018 Tommy Öman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+//    var categoryArray = [Category]()
+//
+//    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categorys
+    
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+
+}
